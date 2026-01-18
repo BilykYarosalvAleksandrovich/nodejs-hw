1026a96e12a004de0885352a797e3f3e1130c01c
Yaroslav Bilyk <bilyk.yaroslav.aleksandrovich@gmail.com>
2026-01-18 17:12:33 +0200

    Update server.js
    ss

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import pino from "pino-http";
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const pino = require('pino-http');
 
 dotenv.config();
 
